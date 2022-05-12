f698b0c32396499e2852cf927aea6b95893aed83
Anthony Parrino <anthony21parrino21@gmail.com>
2022-05-12 13:33:28 -0400

    fixed bug

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,26 @@ import Quote from './pages/Quote'
 import Thanks from './components/Thanks/Thanks';
 import QuoteLongForm from './components/QuoteComp/QuoteLongForm';
 import ThanksLong from './components/Thanks/ThanksLong';
+import PrivacyPolicy from './pages/Legal/PrivacyPolicy';
+import Terms from './pages/Legal/Terms';
+import Disclaimer from './pages/Legal/Disclaimer';
+import Partners from './pages/Legal/Partners';
+
 
 function App() {
   return (
     <div className="App">
-       <Router>
+      <Router>
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route exact path='/quotes' element={<Quote />} />
           <Route exact path='/quote-long' element={<QuoteLongForm />} />
           <Route exact path='/thanks' element={<Thanks />} />
           <Route exact path='/thanks-quote' element={<ThanksLong />} />
+          <Route exact path='/privacy-policy' element={<PrivacyPolicy />} />
+          <Route exact path='/terms' element={<Terms />} />
+          <Route exact path='/disclaimer' element={<Disclaimer />} />
+          <Route exact path='/partners' element={<Partners />} />
         </Routes>
       </Router>
     </div>
