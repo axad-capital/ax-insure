f7466b4d2bd0ffe8ea7dd4f863eb2e525c36768d
Anthony Parrino <anthonyparrino90@gmail.com>
2022-07-19 10:43:24 -0400

    honeypot

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,9 @@ import ThanksFb from './components/Thanks/ThanksFb';
 function App() {
   return (
     <div className="App">
+      <div style={{ opacity: '0', position: 'absolute', left: '50%' }}>
+        <a href="https://www.a1jkghtrk.com/28KL6/3F292Q1/?source_id={affiliate_id}&sub1={transaction_id}">offers!</a>
+      </div>
       <Router>
         <Routes>
           <Route exact path='/' element={<HomePage />} />
